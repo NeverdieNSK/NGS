6bd99015f7d054a060ea0e7c4a19490a4b6c45c2
Anton <darkneverdie@mail.ru>
2020-10-26 00:15:32 +0700

    Добавить в запись в файл текст новости

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ driver.get('https://ngs.ru/text/')
 
 articles = driver.find_elements_by_css_selector('div:nth-child(3)>article')
 news = []
-f = open('data.txt', 'w')
+f = open('data.json', 'w')
 
 for article in articles:
     link = article.find_element_by_tag_name('a').get_attribute('href')
     title = article.find_element_by_css_selector('div>div>h2>a>span').text
-    news.append({'title': title, 'link': link, 'text': None})
+
+
+    text = article.find_element_by_css_selector('div>div>p>a>span').text
+    news.append({'title': title, 'link': link, 'text': text })
 
 
 for n in news:
-    print (n['link'], n['title'])
+    print (n['link'], n['title'], n['text'])
     time.sleep(2)
 
 json.dump(news, f)
